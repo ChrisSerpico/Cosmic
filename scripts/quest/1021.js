a206b1f62016af79066ec80555ac4c60ba9b38ec
Chris Serpico <christopherserpico@gmail.com>
2026-02-05 01:10:01 -0500

    Clean up text for Roger's Apple quest

--- scripts/quest/1021.js
+++ scripts/quest/1021.js
@@ -44,11 +44,11 @@ function start(mode, type, selection) {
         }
 
         if (status == 0) {
-            qm.sendNext("Hey, " + (qm.getPlayer().getGender() == 0 ? "Man" : "Miss") + "~ What's up? Haha! I am Roger who can teach you adorable new Maplers lots of information.");
+            qm.sendNext("Hey, " + (qm.getPlayer().getGender() == 0 ? "man" : "miss") + "~ What's up? Ha! I am Roger, here to teach you adorable new Maplers lots of information.");
         } else if (status == 1) {
-            qm.sendNextPrev("You are asking who made me do this? Ahahahaha!\r\nMyself! I wanted to do this and just be kind to you new travellers.");
+            qm.sendNextPrev("Who made me do this? Aha!\r\nMyself! I just wanted to help you new travellers. Aren't I kind?");
         } else if (status == 2) {
-            qm.sendAcceptDecline("So..... Let me just do this for fun! Abaracadabra~!");
+            qm.sendAcceptDecline("So... let's get started! Abracadabra~!");
         } else if (status == 3) {
             if (qm.getPlayer().getHp() >= 50) {
                 qm.getPlayer().updateHp(25);
@@ -59,9 +59,9 @@ function start(mode, type, selection) {
             }
 
             qm.forceStartQuest();
-            qm.sendNext("Surprised? If HP becomes 0, then you are in trouble. Now, I will give you #rRoger's Apple#k. Please take it. You will feel stronger. Open the Item window and double click to consume. Hey, it's very simple to open the Item window. Just press #bI#k on your keyboard.");
+            qm.sendNext("Surprised? If your HP reaches 0, then you're in trouble. Now, take this #rRoger's Apple#k. Please eat it, you'll feel much better. Open the Item window and double click to consume.\r\n\r\nHow? It's very simple to open the Item window. Just press #bI#k on your keyboard.");
         } else if (status == 4) {
-            qm.sendPrev("Please take all Roger's Apples that I gave you. You will be able to see the HP bar increasing. Please talk to me again when you recover your HP 100%.");
+            qm.sendPrev("Please eat the #rRoger's Apple#k that I gave you. You'll be able to see your HP bar increasing. Please talk to me again when you recover your HP all the way!");
         } else if (status == 5) {
             qm.showInfo("UI/tutorial.img/28");
             qm.dispose();
@@ -86,15 +86,15 @@ function end(mode, type, selection) {
 
         if (status == 0) {
             if (qm.c.getPlayer().getHp() < 50) {
-                qm.sendNext("Hey, your HP is not fully recovered yet. Did you take all the Roger's Apple that I gave you? Are you sure?");
+                qm.sendNext("Hey, your HP is not fully recovered yet! Did you eat the #rRoger's Apple#k that I gave you? Are you sure?");
                 qm.dispose();
             } else {
-                qm.sendNext("How easy is it to consume the item? Simple, right? You can set a #bhotkey#k on the right bottom slot. Haha you didn't know that! right? Oh, and if you are a beginner, HP will automatically recover itself as time goes by. Well it takes time but this is one of the strategies for the beginners.");
+                qm.sendNext("How was your first meal? Delicious, right? You can even set a #bhotkey#k for consumables by dragging one onto the slots in the bottom right. Bet you didn't know that!\r\n\r\nOh, and if you're a beginner, your HP will slowly recover itself as time goes by. You could say this is one of the advantages of being a beginner, ha!");
             }
         } else if (status == 1) {
-            qm.sendNextPrev("Alright! Now that you have learned alot, I will give you a present. This is a must for your travel in Maple World, so thank me! Please use this under emergency cases!");
+            qm.sendNextPrev("Alright! Now that you've learned so much, I'll give you a present. Food is a must for your travel in Maple World, so you better thank me! And don't forget to eat in an emergency!");
         } else if (status == 2) {
-            qm.sendPrev("Okay, this is all I can teach you. I know it's sad but it is time to say good bye. Well take care if yourself and Good luck my friend!\r\n\r\n#fUI/UIWindow.img/QuestIcon/4/0#\r\n#v2010000# 3 #t2010000#\r\n#v2010009# 3 #t2010009#\r\n\r\n#fUI/UIWindow.img/QuestIcon/8/0# 10 exp");
+            qm.sendPrev("Okay, this is all I can teach you. I know it's sad, but it is time to say goodbye. Well... take care of yourself! And good luck, my friend!\r\n\r\n#fUI/UIWindow.img/QuestIcon/4/0#\r\n#v2010000# 3 #t2010000#\r\n#v2010009# 3 #t2010009#\r\n\r\n#fUI/UIWindow.img/QuestIcon/8/0# 10 exp");
         } else if (status == 3) {
             if (qm.isQuestCompleted(1021)) {
                 qm.dropMessage(1, "Unknown Error");
